331be93dfcb902c73ddaa87e5b941a459fba5fee
nikita_pandey <pandeynikita123@gmail.com>
2018-02-24 11:28:04 +0530

    Geocoder for location

--- Gemfile
+++ Gemfile
@@ -56,3 +56,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 #Additional Gems
   gem "twitter-bootstrap-rails"
   gem 'devise'
+  gem 'geocoder'
--- app/models/ambulance.rb
+++ app/models/ambulance.rb
@@ -1,2 +1,8 @@
 class Ambulance < ApplicationRecord
+  geocoded_by :full_address
+  after_validation :geocode
+
+  def full_address
+    "#{self.country} #{self.state} #{self.city} #{self.zipcode}"
+  end
 end
